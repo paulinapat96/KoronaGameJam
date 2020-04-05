eb125c16088fc2ec320c30eb6c8d8b39c2a0b49b
paulinapat96 <paulinapatrowicz@o2.pl>
2020-04-05 22:39:31 +0200

    Added Sounds

---
+++ KoronaJam/Assets/_Game/Scripts/Interactions/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    static SoundManager _instance;
+    [SerializeField] private AudioSource source;
+
+    private void Awake()
+    {
+        _instance = FindObjectOfType<SoundManager>();
+    }
+
+    public static void PlaySound(AudioClip clip)
+    {
+        _instance.source.clip = clip;
+        _instance.source.Play();
+    }
+}
